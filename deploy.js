d7ab203ce7565347cf765b2271199042fbc0df74
Dmitry Sky <dmitry.sky@icloud.com>
2023-01-22 10:05:09 -0700

    Complete contract deploy script

--- deploy.js
+++ deploy.js
@@ -4,7 +4,16 @@ const fs = require("fs");
 
 async function main() {
   const provider = new ethers.provider.JsonRpcProvider(/* url */);
-  const wallet = new ethers.Wallet("private key", provider);
+  const wallet = new ethers.Wallet("private key goes here", provider);
+  const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
+  const binary = fs.readFileSync(
+    "./SimpleStorage_sol_SimpleStorage.bin",
+    "utf8"
+  );
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+  console.log("Deploying, please wait...");
+  const contract = await contractFactory.deploy();
+  contract.log(contract);
 }
 
 main()
