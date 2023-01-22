9e657ef63a0343aacc36575169816cfccaadef59
Dmitry Sky <dmitry.sky@icloud.com>
2023-01-22 09:55:43 -0700

    Add ethersjs

--- deploy.js
+++ deploy.js
@@ -1,4 +1,11 @@
-async function main() {}
+const ethers = require("ethers");
+// read from the .abi and .bin files
+const fs = require("fs");
+
+async function main() {
+  const provider = new ethers.provider.JsonRpcProvider(/* url */);
+  const wallet = new ethers.Wallet("private key", provider);
+}
 
 main()
   .then(() => ProcessingInstruction.exit(0))
