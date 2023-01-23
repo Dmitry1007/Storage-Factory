84611a169f01139f0b6ef480c78794588a9c5a4e
Dmitry Sky <dmitry.sky@icloud.com>
2023-01-23 11:02:54 -0700

    For simplicity, go back to old less secure way

--- deploy.js
+++ deploy.js
@@ -4,13 +4,13 @@ const fs = require("fs");
 
 async function main() {
   const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
-  // const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
-  const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf8");
-  let wallet = new ethers.Wallet.fromEncryptedJsonSync(
-    encryptedJson,
-    process.env.PRIVATE_KEY_PASSWORD
-  );
-  wallet = await wallet.connect(provider);
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+  // const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf8");
+  // let wallet = new ethers.Wallet.fromEncryptedJsonSync(
+  //   encryptedJson,
+  //   process.env.PRIVATE_KEY_PASSWORD
+  // );
+  // wallet = await wallet.connect(provider);
 
   const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
   const binary = fs.readFileSync(
@@ -22,6 +22,7 @@ async function main() {
   const contract = await contractFactory.deploy();
   // console.log(contract);
   await contract.deployTransaction.wait(1);
+  console.log(`Contract Address: ${contract.address}`);
 
   // console.log("deploy with ONLY transaction data (raw)");
   // const nonce = await wallet.getTransactionCount();
